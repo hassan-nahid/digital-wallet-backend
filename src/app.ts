cf32ac22e36e621d72fe4339573142539cd48471
hassan-nahid <scientisthassannahid@gmail.com>
2025-08-25 15:30:55 +0600

    frontend url added

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-  origin: "https://digital-wallet-frontend-one.vercel.app",
+  origin: envVars.FRONTEND_URL,
   credentials: true,
 }));
 // app.use(cors({
