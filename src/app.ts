37605b160cb0700a6c18848349cf845f1f545873
hassan-nahid <scientisthassannahid@gmail.com>
2025-08-25 16:36:34 +0600

    cors origin update

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,14 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use(cors({
-//   origin: "https://digital-wallet-frontend-one.vercel.app",
-//   credentials: true,
-// }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://digital-wallet-frontend-one.vercel.app",
   credentials: true,
 }));
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// }));
 app.use(cookieParser())
 app.use(expressSession({
     secret: envVars.EXPRESS_SESSION_SECRET,
